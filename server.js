08602b4608a5ec2c977fe8e35caec8d20c128072
DiamondStalker <cmoreno981@gmail.com>
2024-09-21 13:40:32 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+const PORT = 3000;
+
+
+app.get('/ping', function (req, res) {
+    res.send('Hello World')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
+
+app.get('/scrape', async (req, res) => {
+    try {
+        // URL que quieres scrapeear
+        const url = 'https://tramites.copnia.gov.co/Copnia_Microsite/RequestStatus/GetRequestInfo?key=9883656&documentNumber=1017256539';
+        const { data } = await axios.get(url);
+
+        const $ = cheerio.load(data);
+
+        res.json({
+            success: true,
+            titles: $('[class="form-control text-box single-line"]').attr('value'),
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al scrapeear la página',
+            error: error.message,
+        });
+    }
+});
